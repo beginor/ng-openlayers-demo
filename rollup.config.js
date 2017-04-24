4818c466b56c0d8ba1d5954a5f0a340e1a71ffbc
beginor <beginor@qq.com>
2017-04-25 07:17:25 +0800

    add ol packages.

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,7 @@ export default {
     nodeResolve({
       jsnext: true,
       module: true,
-      externals: []
+      externals: ['ol']
     }),
     commonjs({
       include: 'node_modules/rxjs/**',
