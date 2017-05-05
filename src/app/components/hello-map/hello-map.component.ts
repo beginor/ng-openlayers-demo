221e8778f3f3bc9bb1c3d8836b62fe5be9aad579
beginor <beginor@qq.com>
2017-05-05 08:12:54 +0800

    add enter leave animation

--- src/app/components/hello-map/hello-map.component.ts
+++ src/app/components/hello-map/hello-map.component.ts
@@ -1,15 +1,34 @@
+import { animate, transition, trigger, state, style } from '@angular/animations';
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'hello-map',
     templateUrl: './hello-map.component.html',
-    styleUrls: ['./hello-map.component.css']
+    styleUrls: ['./hello-map.component.css'],
+    animations: [
+        trigger('state', [
+            transition(':enter', [
+                style({
+                    opacity: 0
+                }),
+                animate(300)
+            ]),
+            transition(':leave', [
+                animate(300),
+                style({
+                    opacity: 0
+                })
+            ])
+        ])
+    ]
 })
 export class HelloMapComponent implements OnInit, AfterViewInit {
 
     @ViewChild('map') mapEl: ElementRef;
     map: ol.Map;
+
+    state: any;
 
     constructor() { }
 
