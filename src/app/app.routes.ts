b9cc6f016448645da9530df9e594f43adccf1c06
beginor <beginor@qq.com>
2017-05-11 09:45:57 +0800

    add vector tile demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,19 @@ import { RouterModule, Route } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { HelloMapComponent } from './components/hello-map/hello-map.component';
+import { VectorTileComponent } from './components/vector-tile/vector-tile.component';
 
 export const RouteComponents = [
     HomeComponent,
-    HelloMapComponent
+    HelloMapComponent,
+    VectorTileComponent
 ];
 
 export const RouteList: RouteWithText[] = [
     { path: '', redirectTo: '/home', pathMatch: 'full', text: '' },
     { path: 'home', component: HomeComponent, text: '' },
-    { path: 'hello-map', component: HelloMapComponent, text: 'Hello, map' }
+    { path: 'hello-map', component: HelloMapComponent, text: 'Hello, map' },
+    { path: 'vector-tile', component: VectorTileComponent, text: 'Vector Tile' }
 ];
 
 export const AppRoutes = RouterModule.forRoot(
---
+++ src/app/components/vector-tile/vector-tile.component.ts
@@ -0,0 +1,82 @@
+import { animate, transition, trigger, state, style } from '@angular/animations';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'vector-tile',
+    templateUrl: './vector-tile.component.html',
+    styleUrls: ['./vector-tile.component.css'],
+    animations: [
+        trigger('state', [
+            transition(':enter', [
+                style({
+                    opacity: 0
+                }),
+                animate(300)
+            ]),
+            transition(':leave', [
+                animate(300),
+                style({
+                    opacity: 0
+                })
+            ])
+        ])
+    ]
+})
+
+export class VectorTileComponent implements OnInit, AfterViewInit {
+
+    @ViewChild('map') mapEl: ElementRef;
+    map: ol.Map;
+
+    state: any;
+    
+    constructor() { }
+
+    ngOnInit() { }
+
+    ngAfterViewInit(): void {
+        var layer = 'postgis:gd_village';
+        var epsg = '900913';
+        this.map = new ol.Map({
+            target: this.mapEl.nativeElement,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM(),
+                }),
+                new ol.layer.VectorTile({
+                    style: (feature: ol.Feature) => {
+                        return new ol.style.Style({
+                            fill: new ol.style.Fill({
+                                color: 'rgba(144, 255, 144, 0.3)'
+                            }),
+                            stroke: new ol.style.Stroke({
+                                color: '#880000',
+                                width: 1
+                            }),
+                            // text: new ol.style.Text({
+                            //     text: feature.get('name'),
+                            //     textAlign: 'center'
+                            // })
+                        })
+                    },
+                    source: new ol.source.VectorTile({
+                        tilePixelRatio: 1, // oversampling when > 1
+                        projection: '',
+                        tileGrid: ol.tilegrid.createXYZ({maxZoom: 19}),
+                        format: new ol.format.MVT({
+                            //featureClass: ol.Feature
+                        }),
+                        url: '/geoserver/gwc/service/tms/1.0.0/' + layer +
+                            '@EPSG:'+epsg+'@pbf/{z}/{x}/{-y}.pbf'
+                    }),
+                    renderOrder: (f1, f2) => 1
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([113.351, 23.1856]),
+                zoom: 14
+            })
+        });
+    }
+}
