6b255dd29fd3bed2ce9022a203e081c5c34c549a
beginor <beginor@qq.com>
2017-10-15 22:38:16 +0800

    show vector tile

--- src/app/components/vector-tile/vector-tile.component.ts
+++ src/app/components/vector-tile/vector-tile.component.ts
@@ -52,39 +52,41 @@ export class VectorTileComponent implements OnInit, AfterViewInit {
                 // new ol.layer.Tile({
                 //     source: new ol.source.OSM(),
                 // }),
-                new ol.layer.VectorTile({
-                    style: (feature: ol.Feature) => {
-                        return new ol.style.Style({
-                            fill: new ol.style.Fill({
-                                color: 'rgba(144, 255, 144, 0.3)'
-                            }),
-                            stroke: new ol.style.Stroke({
-                                color: '#880000',
-                                width: 1
-                            }),
-                            // text: new ol.style.Text({
-                            //     text: feature.get('name'),
-                            //     textAlign: 'center'
-                            // })
-                        });
-                    },
-                    source: new ol.source.VectorTile({
-                        // tilePixelRatio: 1, // oversampling when > 1
-                        projection: 'EPSG:3857',
-                        tileGrid: ol.tilegrid.createXYZ({maxZoom: 19}),
-                        format: new ol.format.MVT({
-                            // featureClass: ol.Feature
-                        }),
-                        url: '/geoserver/gwc/service/tms/1.0.0/' + layer +
-                            '@EPSG:' + epsg + '@pbf/{z}/{x}/{-y}.pbf'
-                    }),
-                    renderOrder: (f1, f2) => 1
-                })
+                // new ol.layer.VectorTile({
+                //     style: (feature: ol.Feature) => {
+                //         return new ol.style.Style({
+                //             fill: new ol.style.Fill({
+                //                 color: 'rgba(144, 255, 144, 0.3)'
+                //             }),
+                //             stroke: new ol.style.Stroke({
+                //                 color: '#880000',
+                //                 width: 1
+                //             }),
+                //             // text: new ol.style.Text({
+                //             //     text: feature.get('name'),
+                //             //     textAlign: 'center'
+                //             // })
+                //         });
+                //     },
+                //     source: new ol.source.VectorTile({
+                //         // tilePixelRatio: 1, // oversampling when > 1
+                //         projection: 'EPSG:3857',
+                //         tileGrid: ol.tilegrid.createXYZ({maxZoom: 19}),
+                //         format: new ol.format.MVT({
+                //             // featureClass: ol.Feature
+                //         }),
+                //         url: '/geoserver/gwc/service/tms/1.0.0/' + layer +
+                //             '@EPSG:' + epsg + '@pbf/{z}/{x}/{-y}.pbf'
+                //     }),
+                //     renderOrder: (f1, f2) => 1
+                // })
             ],
             view: new ol.View({
                 center: ol.proj.fromLonLat([113.351, 23.1856]),
                 zoom: 7
             })
         });
+        olms.apply(this.map, 'assets/vector-tile.json');
+        window.olmap = this.map;
     }
 }
