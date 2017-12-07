7f87801f986e5b3c8089274bbd4c8753315de2c5
beginor <beginor@qq.com>
2017-12-07 09:52:01 +0800

    add water component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,18 +5,25 @@ import { HomeComponent } from './components/home/home.component';
 import {
     VectorTileComponent
 } from './components/vector-tile/vector-tile.component';
+import { WaterComponent } from './components/water/water.component';
 
 export const RouteComponents = [
     HomeComponent,
     HelloMapComponent,
-    VectorTileComponent
+    VectorTileComponent,
+    WaterComponent
 ];
 
 export const RouteList: RouteWithText[] = [
     { path: '', redirectTo: '/home', pathMatch: 'full', text: '' },
     { path: 'home', component: HomeComponent, text: '' },
     { path: 'hello-map', component: HelloMapComponent, text: 'Hello, map' },
-    { path: 'vector-tile', component: VectorTileComponent, text: 'Vector Tile' }
+    {
+        path: 'vector-tile',
+        component: VectorTileComponent,
+        text: 'Vector Tile'
+    },
+    { path: 'water', component: WaterComponent, text: 'Water'}
 ];
 
 export const AppRoutes = RouterModule.forRoot(
---
+++ src/app/components/water/water.component.ts
@@ -0,0 +1,79 @@
+import {
+    AfterViewInit,
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild
+} from '@angular/core';
+
+import MVT from 'ol/format/mvt';
+import Tile from 'ol/layer/tile';
+import VectorTile from 'ol/layer/vectortile';
+import Map from 'ol/map';
+import proj from 'ol/proj';
+import OSM from 'ol/source/osm';
+import VectorTileSource from 'ol/source/vectortile';
+import Fill from 'ol/style/fill';
+import Stroke from 'ol/style/stroke';
+import Style from 'ol/style/style';
+import Text from 'ol/style/text';
+import tileGrid from 'ol/tilegrid';
+import View from 'ol/view';
+
+@Component({
+    selector: 'app-water',
+    templateUrl: './water.component.html',
+    styleUrls: ['./water.component.scss']
+})
+export class WaterComponent implements OnInit, AfterViewInit {
+
+    @ViewChild('map') public mapEl: ElementRef;
+
+    private map: Map;
+
+    constructor() {
+        //
+    }
+
+    public ngOnInit(): void {
+        //
+    }
+
+    public ngAfterViewInit(): void {
+        const layer = 'postgis:gd_river';
+        const epsg = '900913';
+        this.map = new Map({
+            target: this.mapEl.nativeElement,
+            layers: [
+                new Tile({
+                    source: new OSM()
+                }),
+                new VectorTile({
+                    source: new VectorTileSource({
+                        projection: 'EPSG:3857',
+                        format: new MVT(),
+                        url: '/geoserver/gwc/service/tms/1.0.0/' + layer +
+                             '@EPSG:' + epsg + '@pbf/{z}/{x}/{-y}.pbf'
+                    }),
+                    style: (feature) => {
+                        const f = feature;
+                        return new Style({
+                            fill: new Fill({
+                                color: '#238b45'
+                            }),
+                            stroke: new Stroke({
+                                color: '#000000',
+                                width: 1
+                            })
+                        });
+                    }
+                })
+            ],
+            view: new View({
+                center: proj.fromLonLat([113.351, 23.1856]),
+                zoom: 7
+            })
+        });
+    }
+
+}
