6f7f02cbed0c2f6d0e0f168500157bbdcab19a68
beginor <beginor@qq.com>
2017-12-10 16:29:10 +0800

    remove unused code

--- src/app/components/vector-tile/vector-tile.component.ts
+++ src/app/components/vector-tile/vector-tile.component.ts
@@ -50,43 +50,9 @@ export class VectorTileComponent implements OnInit, AfterViewInit {
     }
 
     public ngAfterViewInit(): void {
-        const layer = 'postgis:gd_place_name';
-        const epsg = '900913';
         this.map = new Map({
             target: this.mapEl.nativeElement,
-            layers: [
-                // new ol.layer.Tile({
-                //     source: new ol.source.OSM(),
-                // }),
-                // new ol.layer.VectorTile({
-                //     style: (feature: ol.Feature) => {
-                //         return new ol.style.Style({
-                //             fill: new ol.style.Fill({
-                //                 color: 'rgba(144, 255, 144, 0.3)'
-                //             }),
-                //             stroke: new ol.style.Stroke({
-                //                 color: '#880000',
-                //                 width: 1
-                //             }),
-                //             // text: new ol.style.Text({
-                //             //     text: feature.get('name'),
-                //             //     textAlign: 'center'
-                //             // })
-                //         });
-                //     },
-                //     source: new ol.source.VectorTile({
-                //         // tilePixelRatio: 1, // oversampling when > 1
-                //         projection: 'EPSG:3857',
-                //         tileGrid: ol.tilegrid.createXYZ({maxZoom: 19}),
-                //         format: new ol.format.MVT({
-                //             // featureClass: ol.Feature
-                //         }),
-                //         url: '/geoserver/gwc/service/tms/1.0.0/' + layer +
-                //             '@EPSG:' + epsg + '@pbf/{z}/{x}/{-y}.pbf'
-                //     }),
-                //     renderOrder: (f1, f2) => 1
-                // })
-            ],
+            layers: [],
             view: new View({
                 center: proj.fromLonLat([113.351, 23.1856]),
                 zoom: 7
